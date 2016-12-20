2ed33e98350340b4d594c9fe11deee2e5e233297
Mirco <amirco@gmail.com>
2016-12-20 10:59:08 +0100

    aggiornamento url chiamata ambiente di test

--- src/main/java/it/tn/rivadelgarda/comune/gda/TestWebkit.java
+++ src/main/java/it/tn/rivadelgarda/comune/gda/TestWebkit.java
@@ -12,7 +12,10 @@ public class TestWebkit {
 
         QApplication.initialize(args);
 
-        MyBrowser myBrowser = new MyBrowser("http://google.com");
+//        MyBrowser myBrowser = new MyBrowser("http://chromium.github.io/octane/");
+//        MyBrowser myBrowser = new MyBrowser("https://html5test.com/");
+        // MyBrowser myBrowser = new MyBrowser("http://localhost:8080/gdadocer/index.html");
+        MyBrowser myBrowser = new MyBrowser("http://192.168.64.200:8080/gdadocer/");
         myBrowser.show();
         
         QApplication.instance().exec();
